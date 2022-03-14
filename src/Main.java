fe9632d8a88c96f2a4e421d02a52f5b217b8a77d
d4julian <47072615+d4julian@users.noreply.github.com>
2022-03-14 18:56:07 -0400

    Adding 2 numbers together

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter first number to add: ");
+        int first = scanner.nextInt();
+        System.out.println("Enter second number to add: ");
+        int second = scanner.nextInt();
+
+        System.out.println("Result: " + (first + second));
     }
 }
